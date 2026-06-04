893e94d1163e53fdcc4340ba41eb02b721e17256
Samuel Carriles Baños <samuelcarrilesbanos@gmail.com>
2026-06-04 13:05:29 -0400

    feat(middleware.serialize.jsonista): implement jsonista serializer

---
+++ src/ehr_adapter/middleware/serialize/jsonista.clj
@@ -0,0 +1,18 @@
+(ns ehr-adapter.middleware.serialize.jsonista
+  (:require [ehr-adapter.http.header :refer [json-media-type?]]))
+
+(defn wrap-jsonista
+  ([handler] (wrap-jsonista handler @(requiring-resolve 'jsonista.core/keyword-keys-object-mapper)))
+  ([handler object-mapper]
+   (let [write-string (requiring-resolve 'jsonista.core/write-value-as-string)
+         read-value (requiring-resolve 'jsonista.core/read-value)]
+     (fn [request]
+       (let [req (if (json-media-type? (:content-type request))
+                   (update request :body #(write-string % object-mapper))
+                   request)
+             response (handler req)]
+         (if (json-media-type? (:content-type response))
+           (update response :body #(read-value % object-mapper))
+           response))))))
+
+
